7788668026961781c55811aab6052170e11288ae
natorade247 <natorade247@gmail.com>
2015-07-16 16:49:28 -0400

    Added JSON String Fetching

--- app/src/main/java/edu/acmatucf/stemdayapp/MainActivity.java
+++ app/src/main/java/edu/acmatucf/stemdayapp/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, new FragmentLogin(), "fragment_login")
                 .commit();
+    }
+
+    private String getJSONString(String site) throws Exception {
+        URL url = new URL(site);
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+        String inputLine;
+        StringBuilder str = new StringBuilder(in.readLine());
+        while ((inputLine = in.readLine()) != null)
+            str.append(inputLine);
+        in.close();
+        str.trimToSize();
+        return str.toString();
     }
 
 }
